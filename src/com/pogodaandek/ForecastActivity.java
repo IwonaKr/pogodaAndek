df9478423ff2db3c7c641ecd325370ac18122f79
IwonaKr <iwona.k.lbn@gmail.com>
2013-10-22 21:53:26 +0200

    refaktoryzacja
    czyli chowanie brzydkiego kodu do metod :D

--- src/com/pogodaandek/ForecastActivity.java
+++ src/com/pogodaandek/ForecastActivity.java
@@ -109,229 +109,16 @@ public class ForecastActivity extends Activity {
 			try {
 				JSONObject current_observation = jObject
 						.getJSONObject("current_observation");
-				Log.i("COS", "CURRENT OBS." + current_observation.toString());
-
-				// Obrabianie conditions
-
-				disLoc = new Location2();
-				JSONObject display_location = current_observation
-						.getJSONObject("display_location");
-				disLoc.setFull(display_location.getString("full"));
-				disLoc.setCity(display_location.getString("city"));
-				disLoc.setCountry(display_location.getString("country"));
-				disLoc.setCountry_iso3166(display_location
-						.getString("country_iso3166"));
-				disLoc.setElevation(display_location.getString("elevation"));
-				disLoc.setLatitude(display_location.getString("latitude"));
-				disLoc.setLongitude(display_location.getString("longitude"));
-				Log.i("disLoc",
-						disLoc.getLatitude() + " " + disLoc.getLongitude()
-								+ " " + disLoc.getFull());
-
-				obsLoc = new Location2();
-				JSONObject observation_location = current_observation
-						.getJSONObject("observation_location");
-				obsLoc.setFull(observation_location.getString("full"));
-				obsLoc.setCity(observation_location.getString("city"));
-				obsLoc.setCountry(observation_location.getString("country"));
-				obsLoc.setCountry_iso3166(observation_location
-						.getString("country_iso3166"));
-				obsLoc.setElevation(observation_location.getString("elevation"));
-				obsLoc.setLatitude(observation_location.getString("latitude"));
-				obsLoc.setLongitude(observation_location.getString("longitude"));
-				Log.i("obsLoc",
-						obsLoc.getLatitude() + " " + obsLoc.getLongitude()
-								+ " " + obsLoc.getFull());
-
-				cndtns = new Conditions();
-				cndtns.dewpointC = current_observation.getString("dewpoint_c");
-				cndtns.dewpointString = current_observation
-						.getString("dewpoint_string");
-				cndtns.feelslikeString = current_observation
-						.getString("feelslike_string");
-				cndtns.feelslikeC = current_observation
-						.getString("feelslike_c");
-				cndtns.heatIndexC = current_observation
-						.getString("heat_index_c");
-				cndtns.heatIndexString = current_observation
-						.getString("heat_index_string");
-				cndtns.icon = current_observation.getString("icon");
-				cndtns.iconURL = current_observation.getString("icon_url");
-				cndtns.localTimeRfc822 = current_observation
-						.getString("local_time_rfc822");
-				cndtns.localTzLong = current_observation
-						.getString("local_tz_long");
-				cndtns.localTzOffset = current_observation
-						.getString("local_tz_offset");
-				cndtns.localTzShort = current_observation
-						.getString("local_tz_short");
-				cndtns.observationEpoch = current_observation
-						.getString("observation_epoch");
-				cndtns.observationTime = current_observation
-						.getString("observation_time");
-				cndtns.observationTimeRfc822 = current_observation
-						.getString("observation_time_rfc822");
-				cndtns.pressureMb = current_observation
-						.getString("pressure_mb");
-				cndtns.pressureTrend = current_observation
-						.getString("pressure_trend");
-				cndtns.relativeHumidity = current_observation
-						.getString("relative_humidity");
-				cndtns.tempC = current_observation.getString("temp_c");
-				cndtns.temperatureString = current_observation
-						.getString("temperature_string");
-				cndtns.UV = current_observation.getString("UV");
-				cndtns.visibilityKm = current_observation
-						.getString("visibility_km");
-				cndtns.windChillC = current_observation
-						.getString("windchill_c");
-				cndtns.windChillString = current_observation
-						.getString("windchill_string");
-				cndtns.windDegrees = current_observation
-						.getString("wind_degrees");
-				cndtns.windDir = current_observation.getString("wind_dir");
-				cndtns.windKph = current_observation.getString("wind_kph");
-				cndtns.windMph = current_observation.getString("wind_mph");
-				cndtns.windString = current_observation
-						.getString("wind_string");
-				cndtns.weather = current_observation.getString("weather");
-				Log.i("COS. Conditions:", cndtns.toString() + " "
-						+ cndtns.feelslikeString + " " + cndtns.weather);
-
-				/*
-				 * Obrabianie FORECAST - txtForecast
-				 */
 				JSONObject forecast = jObject.getJSONObject("forecast");
-				Log.i("COS", "FORECAST " + forecast.toString());
 
-				JSONObject txtForecast = forecast.getJSONObject("txt_forecast");
-				JSONArray txtForecastDay = txtForecast
-						.getJSONArray("forecastday");
-				this.txtForecast = new ArrayList<ForecastDay>();
-				for (int i = 0; i < txtForecastDay.length(); i++) {
-					Log.i("forecast", txtForecastDay.getJSONObject(i)
-							.toString());
-					JSONObject tmp = txtForecastDay.getJSONObject(i);
-					ForecastDay dzien = new ForecastDay();
-					dzien.period = tmp.getString("period");
-					dzien.icon = tmp.getString("icon");
-					dzien.iconUrl = tmp.getString("icon_url");
-					dzien.title = tmp.getString("title");
-					dzien.fcttext = tmp.getString("fcttext");
-					dzien.fcttextMetric = tmp.getString("fcttext_metric");
-					dzien.pop = tmp.getString("pop");
-					this.txtForecast.add(dzien);
-				}
-				for (ForecastDay d : this.txtForecast) {
-					Log.i("txtfrcst", d.title + " " + d.fcttextMetric);
-				}
-				Log.i("txt_forecast", txtForecast.toString());
+				obrabianieConditions(current_observation);								
 
-				/*
-				 * Obrabianie FORECAST - simpleforecast
-				 */
-				JSONObject simpleForecast = forecast
-						.getJSONObject("simpleforecast");
-				JSONArray splForecastDay = simpleForecast
-						.getJSONArray("forecastday");
-				this.simpleForecast = new ArrayList<ForecastDay>();
-				for (int i = 0; i < splForecastDay.length(); i++) {
-					Log.i("simple", splForecastDay.getJSONObject(i).toString());
-					JSONObject tmp = splForecastDay.getJSONObject(i);
-					JSONObject tempDate = tmp.getJSONObject("date");
-					Date data = new Date();
-					ForecastDay dzien = new ForecastDay();
-					// pomocnicze zmienne do konwersji ze stringa do int
-					int pom1 = 0;
-					String pom2 = "";
-					JSONObject pom3 = new JSONObject();
+				obrabianieTxtForecast(forecast);
 
-					data.day = tempDate.getString("day");
-					data.epoch = tempDate.getString("epoch");
-					data.hour = tempDate.getString("hour");
-					data.min = tempDate.getString("min");
-					data.month = tempDate.getString("month");
-					data.monthName = tempDate.getString("monthname");
-					data.pretty = tempDate.getString("pretty");
-					data.ampm = tempDate.getString("ampm");
-					data.tzLong = tempDate.getString("tz_long");
-					data.tzShort = tempDate.getString("tz_short");
-					data.weekDay = tempDate.getString("weekday");
-					data.weekDayShort = tempDate.getString("weekday_short");
-					data.yday = tempDate.getString("yday");
-					data.year = tempDate.getString("year");
-					dzien.data = data;
-					dzien.period = tmp.getString("period");
-					dzien.icon = tmp.getString("icon");
-					dzien.iconUrl = tmp.getString("icon_url");
-					dzien.pop = tmp.getString("pop");
-					dzien.conditions = tmp.getString("conditions");
-					dzien.avehumidity = Integer.parseInt(tmp
-							.getString("avehumidity"));
-					dzien.minhumidity = Integer.parseInt(tmp
-							.getString("minhumidity"));
-					dzien.maxhumidity = Integer.parseInt(tmp
-							.getString("maxhumidity"));
-					// wiatr max i ave
-					pom3 = tmp.getJSONObject("maxwind");
-					dzien.maxwind_degrees = pom3.getString("degrees");
-					dzien.maxwind_dir = pom3.getString("dir");
-					dzien.maxwind_kph = Integer.parseInt(pom3.getString("kph"));
-					dzien.maxwind_mph = Integer.parseInt(pom3.getString("mph"));
-					pom3 = tmp.getJSONObject("avewind");
-					dzien.avewind_degrees = pom3.getString("degrees");
-					dzien.avewind_dir = pom3.getString("dir");
-					dzien.avewind_kph = Integer.parseInt(pom3.getString("kph"));
-					dzien.avewind_mph = Integer.parseInt(pom3.getString("mph"));
-					// temperatury i opady
-					pom3 = tmp.getJSONObject("high");
-					dzien.highTempC = pom3.getString("celsius");
-					pom3 = tmp.getJSONObject("low");
-					dzien.lowTempC = pom3.getString("celsius");
-					pom3 = tmp.getJSONObject("qpf_allday");
-					dzien.qpfAllDay = pom3.getString("mm");
-					pom3 = tmp.getJSONObject("snow_allday");
-					dzien.snowAllDay = pom3.getString("cm");
-					this.simpleForecast.add(dzien);
-				}
-				for (ForecastDay d : this.simpleForecast) {
-					Log.i(d.data.day, d.data.pretty + " " + d.conditions);
-				}
-				Log.i("simpleforecast", simpleForecast.toString());
+				obrabianieSimpleForecast(forecast);					
 				
+				obrabianieAstronomii(jObject);
 				
-				
-				/*
-				 * OBRABIANIE DANYCH ZWI¥ZANYCH Z ASTRONOMI¥-S£OÑCE I KSIÊ¯YÆ
-				 */
-				JSONObject astronomy = jObject.getJSONObject("moon_phase");
-				Astronomy astronomia = new Astronomy();
-				astronomia.ageOfMoon = astronomy.getString("ageOfMoon");
-				astronomia.phaseofMoon = astronomy.getString("phaseofMoon");
-
-				JSONObject pom3 = new JSONObject();
-				pom3 = astronomy.getJSONObject("sunrise");
-				Time g = new Time();
-				g.hour = Integer.parseInt(pom3.getString("hour"));
-				g.minute = Integer.parseInt(pom3.getString("minute"));
-				astronomia.moonrise = g;
-				pom3 = astronomy.getJSONObject("sunset");
-				g.hour = Integer.parseInt(pom3.getString("hour"));
-				g.minute = Integer.parseInt(pom3.getString("minute"));
-				astronomia.moonset = g;
-				astronomy = jObject.getJSONObject("sun_phase");
-				pom3 = astronomy.getJSONObject("sunrise");
-				g.hour = Integer.parseInt(pom3.getString("hour"));
-				g.minute = Integer.parseInt(pom3.getString("minute"));
-				astronomia.sunrise = g;
-				pom3 = astronomy.getJSONObject("sunset");
-				g.hour = Integer.parseInt(pom3.getString("hour"));
-				g.minute = Integer.parseInt(pom3.getString("minute"));
-				astronomia.sunset = g;
-				Log.i("CZAS", astronomia.moonrise.hour + " "
-						+ astronomia.moonrise.minute);
-
-				Log.i("astronomy", astronomy.toString());
 			} catch (JSONException e) {
 				// TODO Auto-generated catch block
 				e.printStackTrace();
@@ -339,6 +126,232 @@ public class ForecastActivity extends Activity {
 
 		}
 
+	}
+
+	private void obrabianieConditions(JSONObject current_observation)
+			throws JSONException {
+		// Obrabianie conditions
+		Log.i("COS", "CURRENT OBS." + current_observation.toString());
+		disLoc = new Location2();
+		JSONObject display_location = current_observation
+				.getJSONObject("display_location");
+		disLoc.setFull(display_location.getString("full"));
+		disLoc.setCity(display_location.getString("city"));
+		disLoc.setCountry(display_location.getString("country"));
+		disLoc.setCountry_iso3166(display_location
+				.getString("country_iso3166"));
+		disLoc.setElevation(display_location.getString("elevation"));
+		disLoc.setLatitude(display_location.getString("latitude"));
+		disLoc.setLongitude(display_location.getString("longitude"));
+		Log.i("disLoc",
+				disLoc.getLatitude() + " " + disLoc.getLongitude()
+						+ " " + disLoc.getFull());
+
+		obsLoc = new Location2();
+		JSONObject observation_location = current_observation
+				.getJSONObject("observation_location");
+		obsLoc.setFull(observation_location.getString("full"));
+		obsLoc.setCity(observation_location.getString("city"));
+		obsLoc.setCountry(observation_location.getString("country"));
+		obsLoc.setCountry_iso3166(observation_location
+				.getString("country_iso3166"));
+		obsLoc.setElevation(observation_location.getString("elevation"));
+		obsLoc.setLatitude(observation_location.getString("latitude"));
+		obsLoc.setLongitude(observation_location.getString("longitude"));
+		Log.i("obsLoc",
+				obsLoc.getLatitude() + " " + obsLoc.getLongitude()
+						+ " " + obsLoc.getFull());
+
+		cndtns = new Conditions();
+		cndtns.dewpointC = current_observation.getString("dewpoint_c");
+		cndtns.dewpointString = current_observation
+				.getString("dewpoint_string");
+		cndtns.feelslikeString = current_observation
+				.getString("feelslike_string");
+		cndtns.feelslikeC = current_observation
+				.getString("feelslike_c");
+		cndtns.heatIndexC = current_observation
+				.getString("heat_index_c");
+		cndtns.heatIndexString = current_observation
+				.getString("heat_index_string");
+		cndtns.icon = current_observation.getString("icon");
+		cndtns.iconURL = current_observation.getString("icon_url");
+		cndtns.localTimeRfc822 = current_observation
+				.getString("local_time_rfc822");
+		cndtns.localTzLong = current_observation
+				.getString("local_tz_long");
+		cndtns.localTzOffset = current_observation
+				.getString("local_tz_offset");
+		cndtns.localTzShort = current_observation
+				.getString("local_tz_short");
+		cndtns.observationEpoch = current_observation
+				.getString("observation_epoch");
+		cndtns.observationTime = current_observation
+				.getString("observation_time");
+		cndtns.observationTimeRfc822 = current_observation
+				.getString("observation_time_rfc822");
+		cndtns.pressureMb = current_observation
+				.getString("pressure_mb");
+		cndtns.pressureTrend = current_observation
+				.getString("pressure_trend");
+		cndtns.relativeHumidity = current_observation
+				.getString("relative_humidity");
+		cndtns.tempC = current_observation.getString("temp_c");
+		cndtns.temperatureString = current_observation
+				.getString("temperature_string");
+		cndtns.UV = current_observation.getString("UV");
+		cndtns.visibilityKm = current_observation
+				.getString("visibility_km");
+		cndtns.windChillC = current_observation
+				.getString("windchill_c");
+		cndtns.windChillString = current_observation
+				.getString("windchill_string");
+		cndtns.windDegrees = current_observation
+				.getString("wind_degrees");
+		cndtns.windDir = current_observation.getString("wind_dir");
+		cndtns.windKph = current_observation.getString("wind_kph");
+		cndtns.windMph = current_observation.getString("wind_mph");
+		cndtns.windString = current_observation
+				.getString("wind_string");
+		cndtns.weather = current_observation.getString("weather");
+		Log.i("COS. Conditions:", cndtns.toString() + " "
+				+ cndtns.feelslikeString + " " + cndtns.weather);
+	}
+
+	private void obrabianieTxtForecast(JSONObject forecast)
+			throws JSONException {
+		Log.i("COS", "FORECAST " + forecast.toString());
+		JSONObject txtForecast = forecast.getJSONObject("txt_forecast");
+		JSONArray txtForecastDay = txtForecast
+				.getJSONArray("forecastday");
+		this.txtForecast = new ArrayList<ForecastDay>();
+		for (int i = 0; i < txtForecastDay.length(); i++) {
+			Log.i("forecast", txtForecastDay.getJSONObject(i)
+					.toString());
+			JSONObject tmp = txtForecastDay.getJSONObject(i);
+			ForecastDay dzien = new ForecastDay();
+			dzien.period = tmp.getString("period");
+			dzien.icon = tmp.getString("icon");
+			dzien.iconUrl = tmp.getString("icon_url");
+			dzien.title = tmp.getString("title");
+			dzien.fcttext = tmp.getString("fcttext");
+			dzien.fcttextMetric = tmp.getString("fcttext_metric");
+			dzien.pop = tmp.getString("pop");
+			this.txtForecast.add(dzien);
+		}
+		for (ForecastDay d : this.txtForecast) {
+			Log.i("txtfrcst", d.title + " " + d.fcttextMetric);
+		}
+		Log.i("txt_forecast", txtForecast.toString());
+	}
+
+	private void obrabianieSimpleForecast(JSONObject forecast)
+			throws JSONException {
+		/*
+		 * Obrabianie FORECAST - simpleforecast
+		 */
+		JSONObject simpleForecast = forecast
+				.getJSONObject("simpleforecast");
+		JSONArray splForecastDay = simpleForecast
+				.getJSONArray("forecastday");
+		this.simpleForecast = new ArrayList<ForecastDay>();
+		for (int i = 0; i < splForecastDay.length(); i++) {
+			Log.i("simple", splForecastDay.getJSONObject(i).toString());
+			JSONObject tmp = splForecastDay.getJSONObject(i);
+			JSONObject tempDate = tmp.getJSONObject("date");
+			Date data = new Date();
+			ForecastDay dzien = new ForecastDay();
+			// pomocnicze zmienne do konwersji ze stringa do int
+			int pom1 = 0;
+			String pom2 = "";
+			JSONObject pom3 = new JSONObject();
+
+			data.day = tempDate.getString("day");
+			data.epoch = tempDate.getString("epoch");
+			data.hour = tempDate.getString("hour");
+			data.min = tempDate.getString("min");
+			data.month = tempDate.getString("month");
+			data.monthName = tempDate.getString("monthname");
+			data.pretty = tempDate.getString("pretty");
+			data.ampm = tempDate.getString("ampm");
+			data.tzLong = tempDate.getString("tz_long");
+			data.tzShort = tempDate.getString("tz_short");
+			data.weekDay = tempDate.getString("weekday");
+			data.weekDayShort = tempDate.getString("weekday_short");
+			data.yday = tempDate.getString("yday");
+			data.year = tempDate.getString("year");
+			dzien.data = data;
+			dzien.period = tmp.getString("period");
+			dzien.icon = tmp.getString("icon");
+			dzien.iconUrl = tmp.getString("icon_url");
+			dzien.pop = tmp.getString("pop");
+			dzien.conditions = tmp.getString("conditions");
+			dzien.avehumidity = Integer.parseInt(tmp
+					.getString("avehumidity"));
+			dzien.minhumidity = Integer.parseInt(tmp
+					.getString("minhumidity"));
+			dzien.maxhumidity = Integer.parseInt(tmp
+					.getString("maxhumidity"));
+			// wiatr max i ave
+			pom3 = tmp.getJSONObject("maxwind");
+			dzien.maxwind_degrees = pom3.getString("degrees");
+			dzien.maxwind_dir = pom3.getString("dir");
+			dzien.maxwind_kph = Integer.parseInt(pom3.getString("kph"));
+			dzien.maxwind_mph = Integer.parseInt(pom3.getString("mph"));
+			pom3 = tmp.getJSONObject("avewind");
+			dzien.avewind_degrees = pom3.getString("degrees");
+			dzien.avewind_dir = pom3.getString("dir");
+			dzien.avewind_kph = Integer.parseInt(pom3.getString("kph"));
+			dzien.avewind_mph = Integer.parseInt(pom3.getString("mph"));
+			// temperatury i opady
+			pom3 = tmp.getJSONObject("high");
+			dzien.highTempC = pom3.getString("celsius");
+			pom3 = tmp.getJSONObject("low");
+			dzien.lowTempC = pom3.getString("celsius");
+			pom3 = tmp.getJSONObject("qpf_allday");
+			dzien.qpfAllDay = pom3.getString("mm");
+			pom3 = tmp.getJSONObject("snow_allday");
+			dzien.snowAllDay = pom3.getString("cm");
+			this.simpleForecast.add(dzien);
+		}
+		for (ForecastDay d : this.simpleForecast) {
+			Log.i(d.data.day, d.data.pretty + " " + d.conditions);
+		}
+		Log.i("simpleforecast", simpleForecast.toString());
+	}
+
+	private void obrabianieAstronomii(JSONObject jObject) throws JSONException {
+		/*
+		 * OBRABIANIE DANYCH ZWI¥ZANYCH Z ASTRONOMI¥-S£OÑCE I KSIÊ¯YÆ
+		 */
+		JSONObject astronomy = jObject.getJSONObject("moon_phase");
+		Astronomy astronomia = new Astronomy();
+		astronomia.ageOfMoon = astronomy.getString("ageOfMoon");
+		astronomia.phaseofMoon = astronomy.getString("phaseofMoon");
+
+		JSONObject pom3 = new JSONObject();
+		pom3 = astronomy.getJSONObject("sunrise");
+		Time g = new Time();
+		g.hour = Integer.parseInt(pom3.getString("hour"));
+		g.minute = Integer.parseInt(pom3.getString("minute"));
+		astronomia.moonrise = g;
+		pom3 = astronomy.getJSONObject("sunset");
+		g.hour = Integer.parseInt(pom3.getString("hour"));
+		g.minute = Integer.parseInt(pom3.getString("minute"));
+		astronomia.moonset = g;
+		astronomy = jObject.getJSONObject("sun_phase");
+		pom3 = astronomy.getJSONObject("sunrise");
+		g.hour = Integer.parseInt(pom3.getString("hour"));
+		g.minute = Integer.parseInt(pom3.getString("minute"));
+		astronomia.sunrise = g;
+		pom3 = astronomy.getJSONObject("sunset");
+		g.hour = Integer.parseInt(pom3.getString("hour"));
+		g.minute = Integer.parseInt(pom3.getString("minute"));
+		astronomia.sunset = g;
+		Log.i("CZAS", astronomia.moonrise.hour + " "
+				+ astronomia.moonrise.minute);
+
+		Log.i("astronomy", astronomy.toString());
 	}
 
 	@Override
