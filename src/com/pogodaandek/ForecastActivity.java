539acd7de1890a5650bf91f9f1f1f15320d699c4
IwonaKr <iwona.k.lbn@gmail.com>
2013-10-23 14:26:41 +0200

    dodane
    10 dni prognozy

--- src/com/pogodaandek/ForecastActivity.java
+++ src/com/pogodaandek/ForecastActivity.java
@@ -6,6 +6,7 @@ import java.io.InputStream;
 import java.io.InputStreamReader;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.jar.JarOutputStream;
 
 import org.apache.http.HttpEntity;
 import org.apache.http.HttpResponse;
@@ -26,7 +27,7 @@ import android.widget.TextView;
 
 public class ForecastActivity extends Activity {
 
-	private static String WEATHER_URL = "http://api.wunderground.com/api/c9d15b10ff3ed303/alerts/conditions/forecast/astronomy/hourly/lang:PL/q/Poland/";
+	private static String WEATHER_URL = "http://api.wunderground.com/api/c9d15b10ff3ed303/alerts/conditions/forecast/forecast10day/astronomy/hourly/lang:PL/q/Poland/";
 	public String nazwaMiasta;
 	public String json_string_response = null;
 	public Location2 disLoc = null;
@@ -35,6 +36,8 @@ public class ForecastActivity extends Activity {
 	public List<ForecastDay> txtForecast;
 	public List<ForecastDay> simpleForecast;
 	public List<HourlyForecast> hourlyForecast;
+	public List<ForecastDay> txt10day;
+	public List<ForecastDay> simple10day;
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
@@ -118,68 +121,93 @@ public class ForecastActivity extends Activity {
 
 				obrabianieAstronomii(jObject);
 
+				obrabianieGodzinowej(jObject);
+
+				JSONObject forecast10d = jObject.getJSONObject("forecast");
+
 				/*
-				 * OBRABIANIE DANYCH HOURLY
+				 * Forecast10day txt_forecast
 				 */
-				JSONArray hourly = jObject.getJSONArray("hourly_forecast");
-				HourlyForecast hf = new HourlyForecast();
-				this.hourlyForecast = new ArrayList<HourlyForecast>();
-
-				for (int i = 0; i < hourly.length(); i++) {
-					Log.i("HOURLY " + String.valueOf(i), String.valueOf(i));
+				JSONObject txt10 = forecast10d.getJSONObject("txt_forecast");
+				JSONArray txt10forecast = txt10.getJSONArray("forecastday");
+				this.txt10day = new ArrayList<ForecastDay>();
+				for (int i = 0; i < txt10forecast.length(); i++) {
+					JSONObject tmp=txt10forecast.getJSONObject(i);
+					ForecastDay dzien = new ForecastDay();
+					dzien.period = tmp.getString("period");
+					dzien.icon = tmp.getString("icon");
+					dzien.iconUrl = tmp.getString("icon_url");
+					dzien.title = tmp.getString("title");
+					dzien.fcttext = tmp.getString("fcttext");
+					dzien.fcttextMetric = tmp.getString("fcttext_metric");
+					dzien.pop = tmp.getString("pop");
+					this.txt10day.add(dzien);
+					Log.i(dzien.title, dzien.fcttextMetric);
+				}
+				
+				/*
+				 * Forecast10day simple
+				 */
+				JSONObject smp10=forecast10d.getJSONObject("simpleforecast");
+				JSONArray smp10day = smp10.getJSONArray("forecastday");
+				this.simple10day = new ArrayList<ForecastDay>();
+				for(int i=0; i<smp10.length();i++){
+					JSONObject tmp = smp10day.getJSONObject(i);
+					JSONObject tempDate = tmp.getJSONObject("date");
+					Date data = new Date();
+					ForecastDay dzien = new ForecastDay();
+					// pomocnicze zmienne do konwersji ze stringa do int
+					int pom1 = 0;
+					String pom2 = "";
 					JSONObject pom3 = new JSONObject();
-					JSONObject pom2 = new JSONObject();
-					pom2 = hourly.getJSONObject(i);
-					pom3 = pom2.getJSONObject("FCTTIME");
-					Time pom = new Time();
-					pom.hour = Integer.parseInt(pom3.getString("hour"));
-					pom.second = Integer.parseInt(pom3.getString("sec"));
-					pom.month = Integer.parseInt(pom3.getString("mon"));
-					pom.year = Integer.parseInt(pom3.getString("year"));
-					pom.yearDay = Integer.parseInt(pom3.getString("yday"));
-					pom.monthDay = Integer.parseInt(pom3.getString("mday"));
-					Log.i("MONTH DAY", String.valueOf(pom.monthDay));
-					hf.czas = pom;
 
-					hf.monAbbrev = pom3.getString("mon_abbrev");
-					hf.monthAbbrev = pom3.getString("month_name_abbrev");
-					hf.weekdayNameAbbrev = pom3
-							.getString("weekday_name_abbrev");
-					hf.weekdayNameNight = pom3.getString("weekday_name_night");
-					hf.pretty = pom3.getString("pretty");
-					hf.condition = pom2.getString("condition");
-					pom3 = pom2.getJSONObject("dewpoint");
-					hf.dewpointC = pom3.getString("metric");
-					pom3 = pom2.getJSONObject("temp");
-					hf.tempC = pom3.getString("metric");
-					hf.fctcode = pom2.getString("fctcode");
-					pom3 = pom2.getJSONObject("feelslike");
-					hf.feelslike = pom3.getString("metric");
-					pom3 = pom2.getJSONObject("heatindex");
-					hf.heatindex = pom3.getString("metric");
-					hf.humidity = pom2.getString("humidity");
-					hf.icon = pom2.getString("icon");
-					hf.iconUrl = pom2.getString("icon_url");
-					hf.pop = pom2.getString("pop");
-					pom3 = pom2.getJSONObject("mslp");
-					hf.pressure = pom3.getString("metric");
-					hf.sky = pom2.getString("sky");
-					pom3 = pom2.getJSONObject("wspd");
-					hf.windKph = pom3.getString("metric");
-					pom3 = pom2.getJSONObject("wdir");
-					hf.windDir = pom3.getString("dir");
-					hf.windDegrees = pom3.getString("degrees");
-					pom3 = pom2.getJSONObject("windchill");
-					hf.windchill = pom3.getString("metric");
-					pom3 = pom2.getJSONObject("qpf");
-					hf.qpf = pom3.getString("metric");
-					
-					pom3 = pom2.getJSONObject("snow");
-					hf.snow = pom3.getString("metric");
-
-					hourlyForecast.add(hf);
-					Log.i(String.valueOf(i), hf.feelslike + " "
-							+ hf.czas.monthDay + " " + hf.czas.toString());
+					data.day = tempDate.getString("day");
+					data.epoch = tempDate.getString("epoch");
+					data.hour = tempDate.getString("hour");
+					data.min = tempDate.getString("min");
+					data.month = tempDate.getString("month");
+					data.monthName = tempDate.getString("monthname");
+					data.pretty = tempDate.getString("pretty");
+					data.ampm = tempDate.getString("ampm");
+					data.tzLong = tempDate.getString("tz_long");
+					data.tzShort = tempDate.getString("tz_short");
+					data.weekDay = tempDate.getString("weekday");
+					data.weekDayShort = tempDate.getString("weekday_short");
+					data.yday = tempDate.getString("yday");
+					data.year = tempDate.getString("year");
+					dzien.data = data;
+					dzien.period = tmp.getString("period");
+					dzien.icon = tmp.getString("icon");
+					dzien.iconUrl = tmp.getString("icon_url");
+					dzien.pop = tmp.getString("pop");
+					dzien.conditions = tmp.getString("conditions");
+					dzien.avehumidity = Integer.parseInt(tmp.getString("avehumidity"));
+					dzien.minhumidity = Integer.parseInt(tmp.getString("minhumidity"));
+					dzien.maxhumidity = Integer.parseInt(tmp.getString("maxhumidity"));
+					// wiatr max i ave
+					pom3 = tmp.getJSONObject("maxwind");
+					dzien.maxwind_degrees = pom3.getString("degrees");
+					dzien.maxwind_dir = pom3.getString("dir");
+					dzien.maxwind_kph = Integer.parseInt(pom3.getString("kph"));
+					dzien.maxwind_mph = Integer.parseInt(pom3.getString("mph"));
+					pom3 = tmp.getJSONObject("avewind");
+					dzien.avewind_degrees = pom3.getString("degrees");
+					dzien.avewind_dir = pom3.getString("dir");
+					dzien.avewind_kph = Integer.parseInt(pom3.getString("kph"));
+					dzien.avewind_mph = Integer.parseInt(pom3.getString("mph"));
+					// temperatury i opady
+					pom3 = tmp.getJSONObject("high");
+					dzien.highTempC = pom3.getString("celsius");
+					pom3 = tmp.getJSONObject("low");
+					dzien.lowTempC = pom3.getString("celsius");
+					pom3 = tmp.getJSONObject("qpf_allday");
+					dzien.qpfAllDay = pom3.getString("mm");
+					pom3 = tmp.getJSONObject("snow_allday");
+					dzien.snowAllDay = pom3.getString("cm");
+					this.simple10day.add(dzien);
+				}
+				for (ForecastDay d : this.simpleForecast) {
+					Log.i("10DAYS!!! "+d.data.day, d.data.pretty + " " + d.conditions);
 				}
 
 			} catch (JSONException e) {
@@ -189,6 +217,71 @@ public class ForecastActivity extends Activity {
 
 		}
 
+	}
+
+	private void obrabianieGodzinowej(JSONObject jObject) throws JSONException {
+		/*
+		 * OBRABIANIE DANYCH HOURLY
+		 */
+		JSONArray hourly = jObject.getJSONArray("hourly_forecast");
+		HourlyForecast hf = new HourlyForecast();
+		this.hourlyForecast = new ArrayList<HourlyForecast>();
+
+		for (int i = 0; i < hourly.length(); i++) {
+			Log.i("HOURLY " + String.valueOf(i), String.valueOf(i));
+			JSONObject pom3 = new JSONObject();
+			JSONObject pom2 = new JSONObject();
+			pom2 = hourly.getJSONObject(i);
+			pom3 = pom2.getJSONObject("FCTTIME");
+			Time pom = new Time();
+			pom.hour = Integer.parseInt(pom3.getString("hour"));
+			pom.second = Integer.parseInt(pom3.getString("sec"));
+			pom.month = Integer.parseInt(pom3.getString("mon"));
+			pom.year = Integer.parseInt(pom3.getString("year"));
+			pom.yearDay = Integer.parseInt(pom3.getString("yday"));
+			pom.monthDay = Integer.parseInt(pom3.getString("mday"));
+			Log.i("MONTH DAY", String.valueOf(pom.monthDay));
+			hf.czas = pom;
+
+			hf.monAbbrev = pom3.getString("mon_abbrev");
+			hf.monthAbbrev = pom3.getString("month_name_abbrev");
+			hf.weekdayNameAbbrev = pom3.getString("weekday_name_abbrev");
+			hf.weekdayNameNight = pom3.getString("weekday_name_night");
+			hf.pretty = pom3.getString("pretty");
+			hf.condition = pom2.getString("condition");
+			pom3 = pom2.getJSONObject("dewpoint");
+			hf.dewpointC = pom3.getString("metric");
+			pom3 = pom2.getJSONObject("temp");
+			hf.tempC = pom3.getString("metric");
+			hf.fctcode = pom2.getString("fctcode");
+			pom3 = pom2.getJSONObject("feelslike");
+			hf.feelslike = pom3.getString("metric");
+			pom3 = pom2.getJSONObject("heatindex");
+			hf.heatindex = pom3.getString("metric");
+			hf.humidity = pom2.getString("humidity");
+			hf.icon = pom2.getString("icon");
+			hf.iconUrl = pom2.getString("icon_url");
+			hf.pop = pom2.getString("pop");
+			pom3 = pom2.getJSONObject("mslp");
+			hf.pressure = pom3.getString("metric");
+			hf.sky = pom2.getString("sky");
+			pom3 = pom2.getJSONObject("wspd");
+			hf.windKph = pom3.getString("metric");
+			pom3 = pom2.getJSONObject("wdir");
+			hf.windDir = pom3.getString("dir");
+			hf.windDegrees = pom3.getString("degrees");
+			pom3 = pom2.getJSONObject("windchill");
+			hf.windchill = pom3.getString("metric");
+			pom3 = pom2.getJSONObject("qpf");
+			hf.qpf = pom3.getString("metric");
+
+			pom3 = pom2.getJSONObject("snow");
+			hf.snow = pom3.getString("metric");
+
+			hourlyForecast.add(hf);
+			Log.i(String.valueOf(i), hf.feelslike + " " + hf.czas.monthDay
+					+ " " + hf.czas.toString());
+		}
 	}
 
 	private void obrabianieConditions(JSONObject current_observation)
